602d7feb0f806cf5e3cb5d513f69ffff9a289254
BANANAofMETAL <stefanos_16_12_1994@hotmail.com>
2016-11-21 12:35:58 +0200

    Added login script to all files

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-var islogin = true;
+localStorage.islogin=boolean(true);
 
 function visibility(){
 	
@@ -8,36 +8,58 @@ function visibility(){
 	document.getElementById("totp").style.display="none";
 	document.getElementById("fff").style.display="none";
 }
+function login(){
+	islogin = true;
+	window.location.href = "laGalerie.html";
+}
+function logout(){
+	islogin = false;
+	window.location.href = "laGalerie.html";
+}
 function isLogin(){
 	if (islogin)
 		document.getElementById("signup").outerHTML=""+
-		
-		"<a id=\"signup\"  class=\"button\">"+
-			"<li id=\"signup\" class=\"dropdown\">"+
-				"<a class=\"dropdown-toggle\""+
-				"data-toggle=\"dropdown\" href=\"Categories.html\">Welcome, Admin <span"+
-				"class=\"caret\"></span></a>"+
+		"<div id=\"signup\" class=\"nav navbar-nav navbar-right btn-group-vertical\" role=\"group\">"+
+			"<div class=\"btn-group\" role=\"group\">"+
+				"<button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">"+
+					"Welcome, Admin!"+
+					"<span class=\"caret\"></span>"+
+				"</button>"+
 				"<ul class=\"dropdown-menu\">"+
 					"<li>"+
-						"<a href=\"#\">Cart</a>"+
+						"<a href=\"cart.html\">Cart</a>"+
 					"</li>"+
 					"<li>"+
 						"<a href=\"#\">Order History</a>"+
 					"</li>"+
 					"<li>"+
-						"<a href=\"#\">Settings</a>"+
+						"<a href=\"infoForm.html\">Settings</a>"+
 					"</li>"+
-				"<li>"+
-					"<a href=\"#\">Logout</a>"+
-				"</li>"+
-			"</ul>"+
-		"</li>"+
-		"</a>";
+					"<li>"+
+						"<a onclick=\"logout()\" href=\"#\">Logout</a>"+
+					"</li>"+
+				"</ul>"+
+			"</div>"+
+			"<form>"+
+				"<div class=\"form-group\">"+
+					"<input type=\"text\" class=\"form-control\" placeholder=\"Search...\" />"+
+				"</div>"+
+			"</form>"+
+		"</div>";
 				
 	else
 		document.getElementById("signup").outerHTML=""+
-		"<a id=\"signup\" href=\"SignUp.html\" class=\"button\"> Sign Up/Login "+
-			"<span class=\"glyphicon glyphicon-user\"></a>";
+		"<div id=\"signup\" class=\"nav navbar-nav navbar-right btn-group-vertical\" role=\"group\">"+
+			"<li align=\"right\">"+
+				"<a href=\"SignUp.html\"> Sign Up/Login <span class=\"glyphicon glyphicon-user\"></span></a>"+
+				"<form class=\"navbar-form navbar-right btn-group-vertical\" role=\"group\">"+
+					"<div class=\"form-group\">"+
+						"<input type=\"text\" class=\"form-control\" placeholder=\"Search...\">"+
+					"</div>"+
+					"<a href=\"Search\"><span class=\"glyphicon glyphicon-search\"></span></a>"+
+				"</form>"+
+			"</li>"+
+		"</div>";
 	
 	visibility();	
 }
